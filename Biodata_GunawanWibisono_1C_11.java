13ebbd7952ba1971951f42ca843c8efdc9195933
gunawan885 <gunawanwi376@gmail.com>
2024-09-09 12:47:15 +0700

    gunawan

---
+++ Biodata_GunawanWibisono_1C_11.java
@@ -0,0 +1,14 @@
+package Pertemuan2;
+
+public class Biodata_GunawanWibisono_1C_11 {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Gunawan Wibisono");
+        System.out.println("Kelas   : 1C");
+        System.out.println("Absen   : 11");
+        System.out.println("NIM     : 244107020132");
+        System.out.println("Jurusan : Teknologi Informasi");
+        System.out.println("Prodi   : Teknik Informatika");
+        System.out.println("TTL     : Blitar, 13 Meni 2004");
+    }
+    
+}
