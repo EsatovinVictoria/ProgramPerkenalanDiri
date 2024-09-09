1b1a19c7e7656dea0444e6068f9d074786356a10
EsatovinVictoria <esatovinv@gmail.com>
2024-09-09 09:47:47 +0700

    Perkenalan

--- Topen.java
+++ Topen.java
@@ -4,6 +4,13 @@
 public class Topen {
 
     public static void main(String[] args) {
-        System.out.println("Perkenalan diri maksude");
+        System.out.println("Nama    :  Esatovin Ebenhaezer Victoria ");
+        System.out.println("NIM     :  244107020228");
+        System.out.println("Kelas:  :  1C");
+        System.out.println("No Absen:  08");
+        System.out.println("Prodi   :  D-IV Teknik Informatika");
+        System.out.println("TTL     :  Blitar");
+        System.out.println("No HP   :  083845142800");
+        System.out.println("Hobi    :  Belajar");
     }
 }
