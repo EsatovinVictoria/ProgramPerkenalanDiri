c1cd9c4da4795e4abc1b4e009a0cb108169d31f1
EsatovinVictoria <esatovinv@gmail.com>
2024-09-09 09:50:13 +0700

    Perkenalan

--- Topen.java
+++ Topen.java
@@ -9,7 +9,7 @@ public class Topen {
         System.out.println("Kelas:  :  1C");
         System.out.println("No Absen:  08");
         System.out.println("Prodi   :  D-IV Teknik Informatika");
-        System.out.println("TTL     :  Blitar");
+        System.out.println("TTL     :  Blitar, 6 Januari 2006");
         System.out.println("No HP   :  083845142800");
         System.out.println("Hobi    :  Belajar");
     }
